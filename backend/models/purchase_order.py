06c80d25107b9caf126e845bf0bf525120f3ff5b
Purvanshu Machhi <purvanshu1375@gmail.com>
2025-09-20 17:55:30 +0530

    some bug fixing

--- backend/models/purchase_order.py
+++ backend/models/purchase_order.py
@@ -8,7 +8,7 @@ class PurchaseOrder(db.Model):
     vendor_id = db.Column(db.Integer, db.ForeignKey("contacts.contact_id", ondelete="CASCADE"), nullable=False)
     order_date = db.Column(db.Date, default=date.today, nullable=False)
     status = db.Column(db.String(20), default="Draft")
-    created_by = db.Column(db.Integer, db.ForeignKey("users.user_id", ondelete="CASCADE"), nullable=False)
+    created_by = db.Column(db.String(20), db.ForeignKey("users.user_id", ondelete="CASCADE"), nullable=False)
 
     items = db.relationship("PurchaseOrderItem", backref="purchase_order", cascade="all, delete")
     vendor_bills = db.relationship("VendorBill", backref="purchase_order", cascade="all, delete")
--- backend/models/sales_order.py
+++ backend/models/sales_order.py
@@ -8,7 +8,7 @@ class SalesOrder(db.Model):
     customer_id = db.Column(db.Integer, db.ForeignKey("contacts.contact_id", ondelete="CASCADE"), nullable=False)
     order_date = db.Column(db.Date, default=date.today, nullable=False)
     status = db.Column(db.String(20), default="Draft")
-    created_by = db.Column(db.Integer, db.ForeignKey("users.user_id", ondelete="CASCADE"), nullable=False)
+    created_by = db.Column(db.String(20), db.ForeignKey("users.user_id", ondelete="CASCADE"), nullable=False)
 
     items = db.relationship("SalesOrderItem", backref="sales_order", cascade="all, delete")
     invoices = db.relationship("CustomerInvoice", backref="sales_order", cascade="all, delete")
